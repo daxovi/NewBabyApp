a160ecbdc7067ddc2ce9356d5d007d1f68b1d923
Dalibor Janeček <dalibor.janecek@gmail.com>
2024-10-14 17:26:27 +0200

    Initial Commit

---
+++ NewBabyApp/NewBabyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewBabyAppApp.swift
+//  NewBabyApp
+//
+//  Created by Dalibor Janeček on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewBabyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
